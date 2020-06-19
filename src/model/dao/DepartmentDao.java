1ddab1927ac58ae736b0511c1630cf09d8f12205
Sandra Ribeiro <Sandrarosenia@gmail.com>
2020-06-19 11:34:14 +0100

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public interface DepartmentDao {
+ void insert (Department obj);
+ void update (Department obj);
+ void deleteByID (Integer id);
+ Department findById (Integer id);
+ List<Department> listAll();
+}
